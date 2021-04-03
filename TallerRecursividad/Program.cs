3bc4942c6fdfd1650fa116cd843429097f044dff
sguerreroal <sguerreroal@gmail.com>
2021-04-03 17:53:40 -0500

    Taller recursividad

---
+++ TallerRecursividad/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace TallerRecursividad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int countnumber(int number)
+            {
+                if (number == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return number +countnumber(number -1);
+                }
+            }
+
+            void printcountnumber()
+            {
+                Console.WriteLine("Enter a number");
+                int number = Int32.Parse(Console.ReadLine());
+                if (number>0)
+                {
+                    Console.WriteLine("The total of the numbers is: " + countnumber(number));
+                }                
+            }
+                printcountnumber();
+
+            int reverseCountNumber(int number)
+            {
+                if (number == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return ((number % 10) + reverseCountNumber(number /10));
+                }
+            }
+
+            void PrintReverseCountNumber()
+            {
+                Console.WriteLine("Enter a number");
+                int number = Int32.Parse(Console.ReadLine());
+                if (number > 0)
+                {
+                    Console.WriteLine("The sum of los numbers enters is: " + reverseCountNumber(number));
+                }
+            }
+            PrintReverseCountNumber();
+
+            int numberPrime(int number, int i)
+            {
+                if (i == 1)
+                {
+                    return 1;
+                }
+                else if (number % i == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return numberPrime(number, i - 1);
+                }
+
+            }
+
+            void printNumberPrime()
+            {
+                Console.WriteLine("Enter a number");
+                int number = Int32.Parse(Console.ReadLine());
+                int valnumber = numberPrime(number, (number / 2));
+                if (valnumber == 1)
+                {
+                    Console.WriteLine("The number enter is prime  ");
+                }
+                else
+                {
+                    Console.WriteLine("The number enter is not prime  ");
+                }
+            }
+            printNumberPrime();
+        }
+    }
+}
